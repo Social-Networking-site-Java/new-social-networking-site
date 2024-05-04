acc73515b00fd3a5bd31ef0fcb02359d71b258f5
Akwasititus <www.ayamgatitus@gmail.com>
2024-05-04 22:37:23 +0000

    fix: change passaword

---
+++ src/main/java/com/titus/socialnetworkingsite2/Dto/ChangePasswordRequest.java
@@ -0,0 +1,17 @@
+package com.titus.socialnetworkingsite2.Dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class ChangePasswordRequest {
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
+
+    // Getters and setters
+}
+
